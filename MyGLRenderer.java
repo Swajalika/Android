5ef44db0ff2af66e21fa078168db493fb215c7a8
Swajalika <Swajalika@users.noreply.github.com>
2015-08-09 04:24:43 -0700

    MyGLRenderer.java
    Renders MyGLSurfaceView object

---
+++ MyGLRenderer.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright (C) 2011 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.opengl;
+
+import java.nio.IntBuffer;
+import java.util.ArrayList;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import com.example.android.opengl.Node.TreeNode;
+
+import android.content.Context;
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
+import android.util.Log;
+
+/**
+ * Provides drawing instructions for a GLSurfaceView object. This class
+ * must override the OpenGL ES drawing lifecycle methods:
+ * <ul>
+ *   <li>{@link android.opengl.GLSurfaceView.Renderer#onSurfaceCreated}</li>
+ *   <li>{@link android.opengl.GLSurfaceView.Renderer#onDrawFrame}</li>
+ *   <li>{@link android.opengl.GLSurfaceView.Renderer#onSurfaceChanged}</li>
+ * </ul>
+ */
+public class MyGLRenderer implements GLSurfaceView.Renderer {
+
+    private static final String TAG = "MyGLRenderer";
+    public static final float floatMAX=999999;
+    private Square mSquare;
+    
+    // mMVPMatrix is an abbreviation for "Model View Projection Matrix"
+    private final float[] mMVPMatrix = new float[16];
+    private final float[] mProjectionMatrix = new float[16];
+    private final float[] mViewMatrix = new float[16];
+    public static float ratio;
+    public static float w,h;
+    public static int number=0;             //total number of sub rectangles of main rectangle
+    public TreeNode temp[];
+
+ 
+    public MyGLRenderer(TreeNode[] tmp,int n) {
+    	number=n;
+		temp=tmp;
+	}
+
+	@Override
+    public void onSurfaceCreated(GL10 unused, EGLConfig config) {
+        // Set the background frame color
+        GLES20.glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+        
+        IntBuffer max = IntBuffer.allocate(1);
+        GLES20.glGetIntegerv(GLES20.GL_MAX_TEXTURE_SIZE, max);
+        Log.d("TAGS","Texture units: " + max.get());
+        
+    }
+
+    @Override
+    public void onDrawFrame(GL10 unused) {
+        
+        // Draw background color
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
+
+        // Set the camera position (View matrix)
+        Matrix.setLookAtM(mViewMatrix, 0, 0, 0, -1, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
+        Matrix.setIdentityM(mViewMatrix, 0);
+
+        // Calculate the projection and view transformation
+        Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0);
+
+        // Draw square
+       mSquare.draw(mMVPMatrix);
+
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 unused, int width, int height) {
+        // Adjust the viewport based on geometry changes,
+        // such as screen rotation
+        GLES20.glViewport(0, 0, width, height);
+        w=(float)width;
+        h=(float)height;
+       
+        Matrix.orthoM(mProjectionMatrix, 0, 0, width, 0, height, 0, 1);//width,,height
+        
+        //input children for main rectangle
+        TreeNode root=new TreeNode();
+		
+		root.count = number;
+		root.height=h;
+		root.width=w;
+		root.area = root.height * root.width;
+		root.child=temp;
+		Log.i("TAG","child:"+root.child);
+		for(int l=0;l<number;l++){
+			root.child[l].area=(float) temp[l].area;
+		}
+        root=Node.mainFunction(root);
+        mSquare   = new Square(root);
+       
+    }
+
+    public static int loadShader(int type, String shaderCode){
+
+        // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
+        // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
+        int shader = GLES20.glCreateShader(type);
+
+        // add the source code to the shader and compile it
+        GLES20.glShaderSource(shader, shaderCode);
+        GLES20.glCompileShader(shader);
+
+        return shader;
+    }
+
+    public static void checkGlError(String glOperation) {
+        int error;
+        while ((error = GLES20.glGetError()) != GLES20.GL_NO_ERROR) {
+            Log.e(TAG, glOperation + ": glError " + error);
+            throw new RuntimeException(glOperation + ": glError " + error);
+        }
+    }
+
+
+}
