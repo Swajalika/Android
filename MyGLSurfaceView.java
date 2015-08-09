81a5ba5d5cc7047f39bf6b68a23621a07a23e94e
Swajalika <Swajalika@users.noreply.github.com>
2015-08-09 04:43:40 -0700

    MyGLSurfaceView.java

--- MyGLSurfaceView.java
+++ MyGLSurfaceView.java
@@ -28,14 +28,14 @@ public class MyGLSurfaceView extends GLSurfaceView {
 
     private final MyGLRenderer mRenderer;
 
-    public MyGLSurfaceView(Context context,TreeNode[] tmp,int number) {
+    public MyGLSurfaceView(Context context,int number) {
         super(context);
 
         // Create an OpenGL ES 2.0 context.
         setEGLContextClientVersion(2);
         
         // Set the Renderer for drawing on the GLSurfaceView
-        mRenderer = new MyGLRenderer(tmp,number);
+        mRenderer = new MyGLRenderer(number);
         setRenderer(mRenderer);
 
         // Render the view only when there is a change in the drawing data
